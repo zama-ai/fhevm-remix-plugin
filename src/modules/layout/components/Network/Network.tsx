17c05b628d672cfa981617589716664aaf7774a2
PacificYield <173040337+PacificYield@users.noreply.github.com>
2024-12-04 13:20:47 +0100

    fix: component Network

--- src/modules/layout/components/Network/Network.tsx
+++ src/modules/layout/components/Network/Network.tsx
@@ -1,13 +1,9 @@
-import { useFhevmjs } from '../../../utils';
+import { useFhevmjs } from "../../../utils";
 
 export const Network: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
-  const { created } = useFhevmjs();
-
-  if (!created) {
-    return <p>This network doesn't run fhEVM 0.5.x. Please change to a network using fhEVM 0.5.x.</p>;
-  }
+  useFhevmjs();
 
   return children;
 };
